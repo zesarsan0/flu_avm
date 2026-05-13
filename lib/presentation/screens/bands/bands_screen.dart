23853217b8d8d5c5f694d49a4ed7e313337f6403
cesosa <hola.cesarsoto@gmail.com>
2026-05-14 01:39:06 +0200

    cesosa: Hemos añadido un Pie Chart

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/config/config.dart';
 import 'package:flutter_application_1/presentation/providers/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class BandsScreen extends ConsumerWidget {
   const BandsScreen({super.key});
@@ -17,18 +18,82 @@ final bands = ref.watch(bandsProvider);
       appBar: AppBar(
         title: Text('Bandas'),
       ),
-      body: ListView.builder(
-        itemCount: bands.length,
-        itemBuilder: (context, i){
-          return _bandTile(context, ref, bands[i]);
-        }
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => addereNovumBand(context, ref),
+      body: Column(
+        children: [
+
+_videreData(bands),
+
+const SizedBox(height: 20,),
+
+
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: bands.length,
+              itemBuilder: (context, i){
+                return _bandTile(context, ref, bands[i]);
+              }
+              ),
+          ),
+        ],
+      ),
+        floatingActionButton: Visibility(
+          visible: bands.length <7 ? true : false,
+          child: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => addereNovumBand(context, ref),
+          ),
         ),
     );
   }
+
+Widget _videreData( List<Band> bands ) {
+
+    // ignore: prefer_collection_literals
+  Map<String, double> dataMap = Map(); 
+
+  for (var band in bands) { 
+    dataMap.putIfAbsent(band.nomen, () => band.numerusVotum.toDouble() );
+  }
+
+  final List<Color> colorList = [
+    Colors.pink.shade100,
+    Colors.pink.shade300,
+    Colors.blue.shade200,
+    Colors.blue.shade600,
+    Colors.lightGreen.shade200,
+    Colors.lightGreen.shade600,
+  ];
+  
+  return dataMap.isNotEmpty ? Container(
+    padding: const EdgeInsets.only(left: 5, top: 5),
+    width: double.infinity,
+    height: 200.0,
+    child: PieChart(
+      dataMap: dataMap,
+      animationDuration: const Duration(milliseconds: 800),
+      colorList: colorList,
+      chartType: ChartType.ring,
+      legendOptions: const LegendOptions(
+        showLegendsInRow: false,
+        legendPosition: LegendPosition.right,
+        showLegends: true,
+        legendTextStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontFamily: "CupertinoSystemText", fontSize: 17,
+        ),
+      ),
+      chartValuesOptions: ChartValuesOptions(
+        showChartValues: dataMap.length <= 6, // con más de 6 no se ve el valor
+        showChartValueBackground: true,
+        showChartValuesInPercentage: false,
+        showChartValuesOutside: false,
+      ),
+    ),
+  ) : const LinearProgressIndicator();
+}
+
+
 
 Widget _bandTile(BuildContext context, WidgetRef ref, Band band){
     return Dismissible(
