8e8c1d6e8de25e1a5297ee27858cfeb899bfe6c7
cesosa <hola.cesarsoto@gmail.com>
2026-05-14 00:48:58 +0200

    cesosa: Widget Dismissible para borrar ListView

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/config/config.dart';
+import 'package:go_router/go_router.dart';
 
 class BandsScreen extends StatelessWidget {
   const BandsScreen({super.key});
@@ -15,21 +17,105 @@ class BandsScreen extends StatelessWidget {
         itemBuilder: (context,i){
           return _bandTile(bands[i]);
         }
-        )
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => addereNovumBand(context),
+        ),
     );
   }
 
-  ListTile _bandTile(Band band){
-    return ListTile(
-          leading: CircleAvatar(
-            child: Text(band.nomen.substring(0,2).toUpperCase()),
+Widget _bandTile(Band band){
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction){
+        print('direction: $direction');
+        print('${band.id}');
+      },
+      background: Container(
+        padding: EdgeInsets.only(left:8.0),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text('Delete Band', style: TextStyle(color: Colors.white),),
+        ),
+      ),
+      child: ListTile(
+            leading: CircleAvatar(
+              child: Text(band.nomen.substring(0,2).toUpperCase()),
+            ),
+            title: Text(band.nomen),
+            trailing: Text('${band.numerusVotum}',style: TextStyle(fontSize: 20),),
+            onTap: (){
+              // ignore: avoid_print
+              print(band.nomen);
+            },
           ),
-          title: Text(band.nomen),
-          trailing: Text('${band.numerusVotum}',style: TextStyle(fontSize: 20),),
-          onTap: (){
-            // ignore: avoid_print
-            print(band.nomen);
-          },
-        );
+    );
   }
+
+addereNovumBand(BuildContext context){
+
+final TextEditingController textumController = TextEditingController();
+
+
+
+/*showDialog(
+  context: context, 
+  builder: (context){
+    return AlertDialog(
+      title: Text('New Band Name'),
+content: TextField( controller: textumController,),
+      actions: [
+        MaterialButton(
+        onPressed:() => addereBandAdCollecione(context, textumController.text),
+        textColor: Colors.blue,
+        elevation: 5,
+        child:Text('Add'),
+        )
+      ],
+    );
+  },
+  );
+*/
+
+showCupertinoDialog(
+  context: context, 
+  builder: ( BuildContext context ) => CupertinoAlertDialog(
+    title: const Text('New band name'),
+    content:  CupertinoTextField(
+      controller: textumController,
+      style: TextStyle(
+        color: Theme.of(context).brightness == Brightness.dark 
+          ? Colors.white 
+          : Colors.black
+        )
+      ),
+      actions: [
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          child: const Text('Add'),
+          onPressed: () {
+            addereBandAdCollectione(context, textumController.text);
+            context.pop();
+          }
+        ),
+        CupertinoDialogAction(
+          isDestructiveAction: true,
+          child: const Text('Close'),
+          onPressed: () => context.pop()
+        ),
+      ],
+   )
+);
+
 }
+
+void addereBandAdCollectione (BuildContext context, String nomen){
+  print(nomen);
+  context.pop();
+}
+
+}
+
