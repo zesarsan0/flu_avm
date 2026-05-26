e08e7a398682bba4f23fec56090a3fa4821120b4
Nombre Apellido <alumno@gmail.com>
2026-05-26 19:09:11 +0200

    cesosa: Estado conectado, Texto Websocket y boton modo claro/oscuro

--- lib/presentation/screens/welcome/welcome_screen.dart
+++ lib/presentation/screens/welcome/welcome_screen.dart
@@ -1,18 +1,33 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 //import '../domus/domus_screen.dart'; // cambia esto por tu pantalla real
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends ConsumerWidget {
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final estTenebrisModus = ref.watch(estTenebrisModusProvider);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
 
-          child: Column(children: [_HeaderRow(), _ConnectionRow()]),
+          child: Column(
+            children: [
+              _HeaderRow(
+                estTenebrisModus: estTenebrisModus,
+                onToggle: () {
+                  ref.read(estTenebrisModusProvider.notifier).state =
+                      !estTenebrisModus;
+                },
+              ),
+              _ConnectionRow(),
+              _InfoSection(),
+            ],
+          ),
         ),
       ),
     );
@@ -20,7 +35,10 @@ class WelcomeScreen extends StatelessWidget {
 }
 
 class _HeaderRow extends StatelessWidget {
-  const _HeaderRow();
+  final bool estTenebrisModus;
+  final VoidCallback onToggle;
+
+  const _HeaderRow({required this.estTenebrisModus, required this.onToggle});
 
   @override
   Widget build(BuildContext context) {
@@ -40,17 +58,16 @@ class _HeaderRow extends StatelessWidget {
         const SizedBox(width: 12),
 
         Expanded(
-          child: Text(
-            'Flutter WebSockets',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
+          child: Text('Flu Avm', style: Theme.of(context).textTheme.titleLarge),
         ),
 
         IconButton(
-          onPressed: () {
-            //TODo: cambiar a modo claro/oscuro
-          },
-          icon: const Icon(Icons.dark_mode_outlined),
+          onPressed: onToggle,
+          icon: Icon(
+            estTenebrisModus
+                ? Icons.light_mode_outlined
+                : Icons.dark_mode_outlined,
+          ),
         ),
       ],
     );
@@ -65,9 +82,7 @@ class _ConnectionRow extends StatelessWidget {
     return Column(
       children: [
         // BADGE WS
-
         const SizedBox(height: 30),
-
 
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
@@ -94,7 +109,7 @@ class _ConnectionRow extends StatelessWidget {
               padding: const EdgeInsets.all(16),
 
               decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primaryContainer,
+                color: Theme.of(context).colorScheme.primaryContainer,
 
                 borderRadius: BorderRadius.circular(20),
               ),
@@ -118,7 +133,7 @@ class _ConnectionRow extends StatelessWidget {
               padding: const EdgeInsets.all(16),
 
               decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primaryContainer,
+                color: Theme.of(context).colorScheme.primaryContainer,
 
                 borderRadius: BorderRadius.circular(20),
               ),
@@ -130,6 +145,54 @@ class _ConnectionRow extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ],
+    );
+  }
+}
+
+class _InfoSection extends StatelessWidget {
+  const _InfoSection();
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+        const SizedBox(height: 80),
+
+        // BADGE CONECTADO
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 98, 192, 101),
+            borderRadius: BorderRadius.circular(20),
+          ),
+
+          child: Text(
+            '● CONECTADO',
+            style: Theme.of(context).textTheme.labelSmall?.copyWith(),
+          ),
+        ),
+
+        const SizedBox(height: 20),
+
+        // TITULO
+        Text(
+          'WebSockets en vivo',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+
+        const SizedBox(height: 12),
+
+        // DESCRIPCIÓN
+        Text(
+          'Aprende a construir apps con datos en tiempo real en Flutter. Dos ejemplos prácticos te esperan dentro.',
+          style: textStyles.bodyMedium,
         ),
       ],
     );
